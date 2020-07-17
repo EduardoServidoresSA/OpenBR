035b8315d392f0718b5efaec9acd75f122cd02f4
SERVIDORESSA <55859411+EduardoServidoresSA@users.noreply.github.com>
2020-07-17 12:02:56 -0300

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# https://github.com/Nyr/openvpn-install
+# https://raw.githubusercontent.com/EduardoServidoresSA/OpenBR/master/openvpn-install.sh
 #
 # Copyright (c) 2013 Nyr. Released under the MIT License.
 
